068e294ae6b1186341bb52ad97ff9c0c7a368349
YangMH <yangmh@gemail.yuntech.edu.tw>
2021-03-03 10:12:13 +0800

    add folder predict.

--- src/predict.py
+++ src/predict.py
@@ -1,9 +1,11 @@
 # import
-from PIL import Image
 import torch
 from src.project_parameters import ProjectPrameters
-from src.model import Net, create_model
-from torchvision import transforms
+from src.model import create_model
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader
+import numpy as np
+from PIL import Image
 
 # def
 
@@ -14,13 +16,23 @@ def predict(projectParams):
     model.eval()
 
     # load image and transfrom it
+    result = []
     transform = transforms.Compose([transforms.Resize((projectParams.maxImageSize[0], projectParams.maxImageSize[1])),
                                     transforms.ToTensor()])
-    img = Image.open(projectParams.dataPath).convert('RGB')
-    img = transform(img)[None, :]
-    with torch.no_grad():
-        result = model(img).tolist()[0]
-    return result
+    if '.png' in projectParams.dataPath:
+        img = Image.open(projectParams.dataPath).convert('RGB')
+        img = transform(img)[None, :]
+        with torch.no_grad():
+            result.append(model(img).tolist()[0])
+    else:
+        dataset = datasets.ImageFolder(
+            projectParams.dataPath, transform=transform)
+        dataLoader = DataLoader(dataset, batch_size=projectParams.batchSize,
+                                pin_memory=projectParams.useCuda, num_workers=projectParams.numWorkers)
+        with torch.no_grad():
+            for img, _ in dataLoader:
+                result.append(model(img).tolist())
+    return np.concatenate(result, 0).round(2)
 
 
 if __name__ == '__main__':
